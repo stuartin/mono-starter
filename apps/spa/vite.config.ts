15e3d837a5247abf6448d7eae7c31dc8389aad36
stuartin <13344713+stuartin@users.noreply.github.com>
2025-02-26 13:47:26 -0800

    add proxy to spa

--- apps/spa/vite.config.ts
+++ apps/spa/vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/graphql': 'http://127.0.0.1:8787/graphql'
+		}
+	}
 });
