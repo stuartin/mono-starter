3446b5f1ab4c1635196ff57ee2a6b4511f30ff54
stuartin <13344713+stuartin@users.noreply.github.com>
2025-02-27 22:03:53 -0800

    add redirect on auth error

--- apps/spa/src/lib/graphql/client.ts
+++ apps/spa/src/lib/graphql/client.ts
@@ -1,7 +1,10 @@
 import { PUBLIC_SERVER_ENDPOINT } from '$env/static/public';
-import { Client, debugExchange, fetchExchange, cacheExchange as documentCache } from '@urql/svelte';
+import { Client, debugExchange, fetchExchange, cacheExchange as documentCache, mapExchange } from '@urql/core';
 import { cacheExchange } from "@urql/exchange-graphcache"
 import { devtoolsExchange } from '@urql/devtools';
+import { page } from '$app/state';
+import { ERR } from "@mono/server/src/lib/gql/errors"
+import { goto } from '$app/navigation';
 
 export const client = new Client({
     url: PUBLIC_SERVER_ENDPOINT,
@@ -11,6 +14,16 @@ export const client = new Client({
         devtoolsExchange,
         debugExchange,
         cacheExchange(),
-        fetchExchange
+        mapExchange({
+            onError(error) {
+                const unauthorized = error.graphQLErrors.some((err) => err.extensions.code === ERR.UNAUTHORIZED.extensions.code)
+                if (unauthorized && page.url.pathname !== "/auth/login") {
+                    const url = new URL(`/auth/login?redirect=${page.url.pathname}`, page.url.origin)
+                    goto(url)
+                }
+            },
+        }),
+        fetchExchange,
+
     ],
 });
